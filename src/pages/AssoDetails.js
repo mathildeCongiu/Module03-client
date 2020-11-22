a97cf511c7c2c844a20af62dd534512f57ed79eb
Alex Olle <alex.olle.93@gmail.com>
2020-11-22 18:36:14 +0100

    First version of CSS done

--- src/pages/AssoDetails.js
+++ src/pages/AssoDetails.js
@@ -1,28 +1,30 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import { withAuth } from "../lib/AuthProvider";
-import { Link } from 'react-router-dom'
-import ProfileHeader from '../components/private-common/ProfileHeader';
-
+import { Link } from "react-router-dom";
+import ProfileHeader from "../components/private-common/ProfileHeader";
 
 class AssoDetails extends Component {
-    render() {
-        return (
-            <div>
-                <Link to="">Back</Link>
-                <ProfileHeader /*user={inventeduserfromfunction}*//>
-                
-                <p>Description</p>
+  render() {
+    return (
+      <div className="details-page">
+        <Link className="back-button-details" to="">
+          <img src="/img/delete.png" alt="Back" />
+        </Link>
+        <ProfileHeader /*user={inventeduserfromfunction}*/ />
 
-                <h3>PICK UP</h3>
-                <h4>Adress</h4>
-                <p>Lorem Ipsum, 19</p>
-                <p>08015 BARCELONA</p>
+        <div className="details-info">
+          <p>Description</p>
 
-                <h4>Phone number</h4>
-                <p>654654654</p>
-            </div>
-        )
-    }
+          <h3>PICK UP</h3>
+          <h4>Adress</h4>
+          <p>Lorem Ipsum, 19 <br/> 08015 BARCELONA</p>
+
+          <h4>Phone number</h4>
+          <p>654654654</p>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default withAuth(AssoDetails)
+export default withAuth(AssoDetails);
--- src/pages/BusinessDetail.js
+++ src/pages/BusinessDetail.js
@@ -1,37 +1,39 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import { withAuth } from "../lib/AuthProvider";
-import { Link } from 'react-router-dom'
-import ProductsList from '../components/private-common/ProductsList';
-import ProfileHeader from '../components/private-common/ProfileHeader';
-
+import { Link } from "react-router-dom";
+import ProductsList from "../components/private-common/ProductsList";
+import ProfileHeader from "../components/private-common/ProfileHeader";
 
 class BusinessDetail extends Component {
-    render() {
-        return (
-            <div>
-                <Link to="">Back</Link>
-                <ProfileHeader /*user={inventeduserfromfunction}*//>
-                
-                <p>Description</p>
+  render() {
+    return (
+      <div className="details-page asso-details">
+        <Link className="back-button-details" to="">
+          <img src="/img/delete.png" alt="Back" />{" "}
+        </Link>
+        <ProfileHeader /*user={inventeduserfromfunction}*/ />
 
-                <h3>PICK UP</h3>
-                <h4>Adress</h4>
-                <p>Lorem Ipsum, 19</p>
-                <p>08015 BARCELONA</p>
+        <div className="details-info">
+          <p>Description</p>
 
-                <h4>Phone number</h4>
-                <p>654654654</p>
+          <h3>PICK UP</h3>
+          <h4>Adress</h4>
+          <p>Lorem Ipsum, 19, <br/> 08015 BARCELONA</p>
 
-                <h4>Schedule</h4>
-                <p>WED 20h</p>
+          <h4>Phone number</h4>
+          <p>654654654</p>
 
-                <h3>PRODUCTS</h3>
-                <ProductsList />
+          <h4>Schedule</h4>
+          <p>WED 20h</p>
 
-                <button>Request partnership</button>
-            </div>
-        )
-    }
+          <h3>PRODUCTS</h3>
+          <ProductsList />
+        </div>
+
+        <button className="button add-edit-product-button">Request partnership</button>
+      </div>
+    );
+  }
 }
 
-export default withAuth(BusinessDetail)
+export default withAuth(BusinessDetail);
