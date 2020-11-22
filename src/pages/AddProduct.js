482227f41a9a4de9acbd476d9327ae73324ca878
Alex Olle <alex.olle.93@gmail.com>
2020-11-22 17:30:59 +0100

    Business profile css done

--- src/pages/AddProduct.js
+++ src/pages/AddProduct.js
@@ -1,26 +1,32 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import { withAuth } from "../lib/AuthProvider";
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 class AddProduct extends Component {
-    render() {
-        return (
-            <div>
-                <Link><img src="" alt="Back"/></Link>
-                <form>
-                    <label>Name</label>
-                    <input type="text" name="name" />
+  render() {
+    return (
+      <div className="add-edit-product">
+        <Link to="/products" className="back-button">
+          <img src="/img/delete.png" alt="Back" />
+        </Link>
+        <form className="add-edit-form">
+          <label>Name</label>
+          <input type="text" name="name" />
 
-                    <label>Type</label>
-                    <select name="productType" >
-                        <option value="fresh">Fresh food</option>
-                        <option value="can">Canned food</option>
-                    </select>
-                    <input type="submit" value="Add Product"/>
-                </form>
-            </div>
-        )
-    }
+          <label>Type</label>
+          <select name="productType">
+            <option value="fresh">Fresh food</option>
+            <option value="can">Canned food</option>
+          </select>
+          <input
+            className="button add-edit-product-button"
+            type="submit"
+            value="Add Product"
+          />
+        </form>
+      </div>
+    );
+  }
 }
 
-export default withAuth(AddProduct)
+export default withAuth(AddProduct);
