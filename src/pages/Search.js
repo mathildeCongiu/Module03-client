d7d31ea221c0d193e67f9da34c22a0b2e1773ded
Alex Olle <alex.olle.93@gmail.com>
2020-11-22 18:00:30 +0100

    Searc CSS done

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,48 +1,73 @@
-import React, { Component } from 'react'
-import CardList from '../components/private-common/CardList';
+import React, { Component } from "react";
+import CardList from "../components/private-common/CardList";
 import { withAuth } from "../lib/AuthProvider";
+import Navbar from "../components/private-common/Navbar";
 
 class Search extends Component {
-    state = {
-        areFiltersShown: false,
-    }
-    // onChangeHandler = (event) => {
-    //     let value = event.target.value
-    //     this.props.showResult(value)
+  state = {
+    areFiltersShown: false,
+  };
+  // onChangeHandler = (event) => {
+  //     let value = event.target.value
+  //     this.props.showResult(value)
+  // }
+
+  showFilters = () => {
+    this.setState({
+      areFiltersShown: !this.state.areFiltersShown,
+    });
+  };
+
+  render() {
+    // {
+    //   console.log(this.props.search());
     // }
 
-    showFilters = () => {
-        this.setState({
-            areFiltersShown: !this.state.areFiltersShown
-        })
-    }
+    return (
+      <div className="search-page">
+        <div className="search-input">
+          <label>SEARCH</label>
+          <input
+            type="text"
+            name="search" /*onChange= { (event) => this.onChangeHandler(event) } */
+          />
+        </div>
+        <button className="button filter-button" onClick={this.showFilters}>FILTERS</button>
+        {this.state.areFiltersShown ? (
+          <div className="filter-options">
+            <label>
+              <input type="checkbox" value="bakery" />
+              Bakery
+            </label>
+            <label>
+              <input type="checkbox" value="fishmonger" />
+              Fishmonger
+            </label>
+            <label>
+              <input type="checkbox" value="supermarket" />
+              Supermarket
+            </label>
+            <label>
+              <input type="checkbox" value="butchShop" />
+              Butcher Shop
+            </label>
+            <label>
+              <input type="checkbox" value="restaurant" />
+              Restaurant
+            </label>
+            <label>
+              <input type="checkbox" value="grocery" />
+              Green Grocery
+            </label>
+          </div>
+        ) : null}
 
-    render() {
-        {console.log(this.props.search())}
-
-        return (
-            
-            <div>
-                <div>
-                    <label>Search</label>
-                    <input type="text" name="search" /*onChange= { (event) => this.onChangeHandler(event) } *//>
-                </div>
-                <button onClick={this.showFilters}>Filters</button>
-                {this.state.areFiltersShown ? <div>
-                    <label><input type="checkbox" value="bakery" />Bakery</label>
-                    <label><input type="checkbox" value="fishmonger" />Fishmonger</label>
-                    <label><input type="checkbox" value="supermarket" />Supermarket</label>
-                    <label><input type="checkbox" value="butchShop" />Butcher Shop</label>
-                    <label><input type="checkbox" value="restaurant" />Restaurant</label>
-                    <label><input type="checkbox" value="grocery" />Green Grocery</label>
-
-                </div> : null }
-
-                <CardList />
-                {/* {this.props.search().data.map(element => console.log(element) )} */}
-            </div>
-        )
-    }
+        <CardList />
+        {/* {this.props.search().data.map(element => console.log(element) )} */}
+        <Navbar />
+      </div>
+    );
+  }
 }
 
-export default withAuth(Search)
+export default withAuth(Search);
