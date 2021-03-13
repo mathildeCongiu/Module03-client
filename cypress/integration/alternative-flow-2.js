4da475cc6bf2f0013980156a7568e7d67a8dcd64
Mathilde Congiu <mathilde.congiu@hotmail.com>
2021-03-13 12:18:32 +0100

    No substancial changes

---
+++ cypress/integration/alternative-flow-2.js
@@ -0,0 +1,39 @@
+describe("Alternative flow 4A: Association search for a specific business", () => {
+  it(`Visits the Volunt'Hero Home Page`, () => {
+    cy.visit("http://localhost:4000");
+  });
+
+  it('finds and click the link "Log In"', () => {
+    cy.get("a").get(".header-link").contains("Log In").click();
+  });
+
+  it('finds and click the link "Log In as an Association"', () => {
+    cy.get("button").contains("Log In as an Association").click();
+  });
+
+  it("fill and send log in form", () => {
+    cy.get('input[name="email"]').type("amir@asso.com");
+    cy.get('input[name="password"]').type("1234");
+    cy.get("form").submit();
+  });
+
+  it('finds and click the link "Search"', () => {
+    cy.get("p").contains("Search").click();
+  });
+
+  it('writes "Bakery Agustina Bonita" in the search bar', () => {
+    cy.get('input[name="search"]').type("Bakery Agustina Bonita");
+  });
+
+  it('finds and click "Bakery Agustina Bonita"', () => {
+    cy.get("h3").contains("Bakery Agustina Bonita").click();
+  });
+
+  it('finds and click "Request partnership" button', () => {
+    cy.get("button").contains("Request partnership").click();
+  });
+});
+
+/*
+FIX CORS PROBLEM
+*/
