cf0c44b4d9de27022ce3eec473ffc5fb4350428a
Alex Olle <alex.olle.93@gmail.com>
2021-03-15 09:13:58 +0100

    Fix writing

--- cypress/integration/alternative-flow-2.js
+++ cypress/integration/alternative-flow-2.js
@@ -3,22 +3,22 @@ describe("Alternative Flow 4B: Association search for a specific business with f
     cy.visit("http://volunthero.herokuapp.com");
   });
 
-  it('finds and click the link "Log In"', () => {
+  it('finds and clicks the link "Log In"', () => {
     cy.get("a").get(".header-link").contains("Log In").click();
   });
 
-  it('finds and click the link "Log In as an Association"', () => {
+  it('finds and clicks the link "Log In as an Association"', () => {
     cy.get("button").contains("Log In as an Association").click();
   });
 
-  it("fill and send log in form", () => {
+  it("fills and sends log in form", () => {
     cy.get('input[name="email"]').type("rakuten@asso.com");
     cy.get('input[name="password"]').type("1234");
     cy.get("form").submit();
     cy.url().should("include", "/dashboard");
   });
 
-  it('finds and click the link "Search"', () => {
+  it('finds and clicks the link "Search"', () => {
     cy.server();
     cy.intercept("GET", "/association/search").as("business");
     cy.get("p").contains("Search").click();
@@ -31,17 +31,17 @@ describe("Alternative Flow 4B: Association search for a specific business with f
     cy.expect("@businessArr").to.have.length.greaterThan(0);
   });
 
-  it('clicks on "FILTERS" and select "Bakery" options', () => {
+  it('clicks on "FILTERS" and selects "Bakery" options', () => {
     cy.get("button").contains("FILTERS").click();
     cy.get('input[value="bakery"]').click();
   });
 
-  it('finds and click "Bakery Augustina Bonita"', () => {
+  it('finds and clicks "Bakery Augustina Bonita"', () => {
     cy.get("h3").contains("Bakery Augustina Bonita").parent().click();
     cy.url().should("include", "/business-details");
   });
 
-  it('finds and click "Request partnership" button', () => {
+  it('finds and clicks "Request partnership" button', () => {
     cy.server();
     cy.intercept("POST", "/association/business/5fbf855a729aed0017857024", {
       statusCode: 200,
